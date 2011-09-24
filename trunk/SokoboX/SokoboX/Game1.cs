5cd611516ee036c15575a2dcd376b9fe3191b2a6
markosthiago@gmail.com <markosthiago@gmail.com@abea603c-5ac2-9416-4f93-96eae0228db9>
2011-09-24 01:33:59 +0000

    Menus Adicionados, Mapa Extra, Imas

--- trunk/SokoboX/SokoboX/Game1.cs
+++ trunk/SokoboX/SokoboX/Game1.cs
@@ -17,6 +17,7 @@ namespace SokoboX
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         TileMap map1;
+        Menu menu = new Menu();
         Tile tileSet = new Tile();
         int squaresAcross = 20;
         int squaresDown = 15;
@@ -25,6 +26,8 @@ namespace SokoboX
         KeyboardState keyboardState, previousState;
         Texture2D pullMag, pushMag;
         SoundManager sound;
+        enum Screens { MENU, GAME, OPTIONS };
+        Screens currentScreen = Screens.MENU;
         string World = "grass";
 
         public Game1()
@@ -42,6 +45,7 @@ namespace SokoboX
             MapArrays.mapListInit();
             map1 = new TileMap(currentMap);
             map1.initializeMap();
+            menu.Initialize(this.Content);
             base.Initialize();
         }
 
@@ -86,118 +90,137 @@ namespace SokoboX
         {
             keyboardState = Keyboard.GetState();
 
-            if (keyboardState.IsKeyDown(Keys.Escape)) this.Exit();
-
-            if (keyboardState.IsKeyDown(Keys.M))
+            if (currentScreen == Screens.GAME)
             {
-                if (sound.playing != true)
-                {
-                    sound.playing = true;
-                }
-                else
-                { sound.playing = false; }
-                sound.playSong();
-            }
+                #region GameUpdate
 
-            if ((keyboardState.IsKeyDown(Keys.R)) && (previousState.IsKeyUp(Keys.R))) 
-            {
-                map1 = new TileMap(currentMap);
-                map1.initializeMap();
-                carregaTexturaCaixas();
-                if (currentMap > 3)  //TESTE
+
+                if (keyboardState.IsKeyDown(Keys.Escape)) this.Exit();
+
+                if (keyboardState.IsKeyDown(Keys.M))
                 {
-                    World = "metal";
-                    tileSet.texture = Content.Load<Texture2D>("Graphics/Industrial/tileset");
-                    foreach (Box box in map1.boxList)
+                    if (sound.playing != true)
                     {
-                        box.boxTexture = Content.Load<Texture2D>("Graphics/Industrial/box");
+                        sound.playing = true;
                     }
+                    else
+                    { sound.playing = false; }
+                    sound.playSong();
                 }
-            }
 
-            if ((keyboardState.IsKeyDown(Keys.N)) && (previousState.IsKeyUp(Keys.N)) && (currentMap < MapArrays.limite())) //FUNÇÃO DE DEBUG - TROCA PARA O PRÓXIMO MAPA
-            {
-                currentMap++;
-                if (currentMap <= MapArrays.limite())
+                if ((keyboardState.IsKeyDown(Keys.R)) && (previousState.IsKeyUp(Keys.R)))
                 {
                     map1 = new TileMap(currentMap);
                     map1.initializeMap();
                     carregaTexturaCaixas();
-                    if (currentMap > 3) 
+                    if (currentMap > 3)  //TESTE
                     {
-                        //soundEffect = Content.Load<SoundEffect>("Sounds/Industrial/metal_drag");
-                        sound.soundLoad("metal");
+                        World = "metal";
                         tileSet.texture = Content.Load<Texture2D>("Graphics/Industrial/tileset");
                         foreach (Box box in map1.boxList)
                         {
                             box.boxTexture = Content.Load<Texture2D>("Graphics/Industrial/box");
                         }
-
                     }
                 }
-            }
 
-            if (!Player.caixa)
-            {
-                if (Player.movendo)
+                if ((keyboardState.IsKeyDown(Keys.N)) && (previousState.IsKeyUp(Keys.N)) && (currentMap < MapArrays.limite())) //FUNÇÃO DE DEBUG - TROCA PARA O PRÓXIMO MAPA
                 {
-                    switch (Player.playerFacing)
+                    currentMap++;
+                    if (currentMap <= MapArrays.limite())
                     {
-                        case Player.facing.LEFT:  Player.moveLeft(map1);  break;
-                        case Player.facing.RIGHT: Player.moveRight(map1); break;
-                        case Player.facing.UP:    Player.moveUp(map1);    break;
-                        case Player.facing.DOWN:  Player.moveDown(map1);  break;
+                        map1 = new TileMap(currentMap);
+                        map1.initializeMap();
+                        carregaTexturaCaixas();
+                        if (currentMap > 3)
+                        {
+                            //soundEffect = Content.Load<SoundEffect>("Sounds/Industrial/metal_drag");
+                            sound.soundLoad("metal");
+                            tileSet.texture = Content.Load<Texture2D>("Graphics/Industrial/tileset");
+                            foreach (Box box in map1.boxList)
+                            {
+                                box.boxTexture = Content.Load<Texture2D>("Graphics/Industrial/box");
+                            }
+
+                        }
+                    }
+                }
+
+                if (!Player.caixa)
+                {
+                    if (Player.movendo)
+                    {
+                        switch (Player.playerFacing)
+                        {
+                            case Player.facing.LEFT: Player.moveLeft(map1); break;
+                            case Player.facing.RIGHT: Player.moveRight(map1); break;
+                            case Player.facing.UP: Player.moveUp(map1); break;
+                            case Player.facing.DOWN: Player.moveDown(map1); break;
+                        }
+                    }
+                    else
+                    {
+                        if (keyboardState.IsKeyDown(Keys.Up)) { Player.moveUp(map1); Player.movendo = true; }
+                        else
+                            if (keyboardState.IsKeyDown(Keys.Down)) { Player.moveDown(map1); Player.movendo = true; }
+                            else
+                                if (keyboardState.IsKeyDown(Keys.Left)) { Player.moveLeft(map1); Player.movendo = true; }
+                                else
+                                    if (keyboardState.IsKeyDown(Keys.Right)) { Player.moveRight(map1); Player.movendo = true; }
                     }
                 }
                 else
                 {
-                    if (keyboardState.IsKeyDown(Keys.Up))    { Player.moveUp(map1);    Player.movendo = true; } else
-                    if (keyboardState.IsKeyDown(Keys.Down))  { Player.moveDown(map1);  Player.movendo = true; } else 
-                    if (keyboardState.IsKeyDown(Keys.Left))  { Player.moveLeft(map1);  Player.movendo = true; } else
-                    if (keyboardState.IsKeyDown(Keys.Right)) { Player.moveRight(map1); Player.movendo = true; }
-                }
-            }
-            else
-            {
-                if (podeMoverCaixa())
-                {
-                    switch (Player.playerFacing)
+                    if (podeMoverCaixa())
                     {
-                        case Player.facing.LEFT:
-                            Player.caixaAtual.moveLeft(map1);
-                            Player.moveLeft(map1);
-                            break;
-                        case Player.facing.RIGHT:
-                            Player.caixaAtual.moveRight(map1);
-                            Player.moveRight(map1);
-                            break;
-                        case Player.facing.UP:
-                            Player.caixaAtual.moveUp(map1);
-                            Player.moveUp(map1);
-                            break;
-                        case Player.facing.DOWN:
-                            Player.caixaAtual.moveDown(map1);
-                            Player.moveDown(map1);
-                            break;
+                        switch (Player.playerFacing)
+                        {
+                            case Player.facing.LEFT:
+                                Player.caixaAtual.moveLeft(map1);
+                                Player.moveLeft(map1);
+                                break;
+                            case Player.facing.RIGHT:
+                                Player.caixaAtual.moveRight(map1);
+                                Player.moveRight(map1);
+                                break;
+                            case Player.facing.UP:
+                                Player.caixaAtual.moveUp(map1);
+                                Player.moveUp(map1);
+                                break;
+                            case Player.facing.DOWN:
+                                Player.caixaAtual.moveDown(map1);
+                                Player.moveDown(map1);
+                                break;
+                        }
+
+                        spriteBatch.Begin();
+                        Player.caixaAtual.Draw(spriteBatch);
+                        spriteBatch.End();
                     }
-
-                    spriteBatch.Begin();
-                    Player.caixaAtual.Draw(spriteBatch);
-                    spriteBatch.End();
+                    else
+                    {
+                        Player.caixa = false;
+                    }
                 }
-                else
+
+                foreach (Magnet magnet in map1.magnetList)
                 {
-                    Player.caixa = false;
+                    magnet.UpdateMagnet(map1);
+                }
+                #endregion
+            }
+
+            if (currentScreen == Screens.MENU)
+            {
+                menu.Update(keyboardState, previousState);
+                if (menu.Selected == Menu.Selection.START)
+                {
+                    currentScreen = Screens.GAME;
                 }
             }
-
-            foreach (Magnet magnet in map1.magnetList)
-            {
-                magnet.UpdateMagnet(map1);
-            }
-
             previousState = keyboardState;
 
+            
             base.Update(gameTime);
         }
 
@@ -235,68 +258,78 @@ namespace SokoboX
 
             spriteBatch.Begin();
 
-            #region DrawTileMap
-            
-            Vector2 firstSquare = new Vector2(Camera.position.X / 32, Camera.position.Y / 32);
-            int firstX = (int)firstSquare.X;
-            int firstY = (int)firstSquare.Y;
-
-            Vector2 squareOffset = new Vector2(Camera.position.X % 32, Camera.position.Y % 32);
-            int offsetX = (int)squareOffset.X;
-            int offsetY = (int)squareOffset.Y;
-
-            for (int y = 0; y < squaresDown; y++)
+            if (currentScreen == Screens.GAME)
             {
-                for (int x = 0; x < squaresAcross; x++)
+                #region DrawGame
+
+                #region DrawTileMap
+
+                Vector2 firstSquare = new Vector2(Camera.position.X / 32, Camera.position.Y / 32);
+                int firstX = (int)firstSquare.X;
+                int firstY = (int)firstSquare.Y;
+
+                Vector2 squareOffset = new Vector2(Camera.position.X % 32, Camera.position.Y % 32);
+                int offsetX = (int)squareOffset.X;
+                int offsetY = (int)squareOffset.Y;
+
+                for (int y = 0; y < squaresDown; y++)
                 {
-                    spriteBatch.Draw(
-                    tileSet.texture,
-                    new Rectangle((x * 32) - offsetX, (y * 32) - offsetY, 32, 32),
-                    tileSet.GetSourceRectangle(map1.Rows[y + firstY].Columns[x + firstX].TileID),
-                    Color.White);
-                }
-            }
-
-            #endregion
-
-            foreach (Box box in map1.boxList)
-            {
-                if (box.boxTexture != null) box.Draw(spriteBatch);
-            }
-
-            foreach (Magnet magnet in map1.magnetList)
-            {
-                if (magnet.texture != null) magnet.Draw(spriteBatch);
-            }
-
-            Player.drawPlayer(spriteBatch);
-
-            if (Player.caixaAtual != null)
-            {
-                if (Player.caixaAtual.verificaFimDeJogo(map1))
-                {
-                    delay(3);
-                    currentMap++;
-                    if (currentMap < MapArrays.limite())
+                    for (int x = 0; x < squaresAcross; x++)
                     {
-                        map1 = new TileMap(currentMap);
-                        map1.initializeMap();
-                        carregaTexturaCaixas();
-                        if (currentMap > 3)  //TESTE
-                        {
-                            //soundEffect = Content.Load<SoundEffect>("Sounds/Industrial/metal_drag");
-                            tileSet.texture = Content.Load<Texture2D>("Graphics/Industrial/tileset");
-                            foreach (Box box in map1.boxList)
-                            {
-                                box.boxTexture = Content.Load<Texture2D>("Graphics/Industrial/box");
-                            }
-
-                        }
+                        spriteBatch.Draw(
+                        tileSet.texture,
+                        new Rectangle((x * 32) - offsetX, (y * 32) - offsetY, 32, 32),
+                        tileSet.GetSourceRectangle(map1.Rows[y + firstY].Columns[x + firstX].TileID),
+                        Color.White);
                     }
                 }
 
+                #endregion
+
+                foreach (Box box in map1.boxList)
+                {
+                    if (box.boxTexture != null) box.Draw(spriteBatch);
+                }
+
+                foreach (Magnet magnet in map1.magnetList)
+                {
+                    if (magnet.texture != null) magnet.Draw(spriteBatch);
+                }
+
+                Player.drawPlayer(spriteBatch);
+
+                if (Player.caixaAtual != null)
+                {
+                    if (Player.caixaAtual.verificaFimDeJogo(map1))
+                    {
+                        delay(3);
+                        currentMap++;
+                        if (currentMap < MapArrays.limite())
+                        {
+                            map1 = new TileMap(currentMap);
+                            map1.initializeMap();
+                            carregaTexturaCaixas();
+                            if (currentMap > 3)  //TESTE
+                            {
+                                //soundEffect = Content.Load<SoundEffect>("Sounds/Industrial/metal_drag");
+                                tileSet.texture = Content.Load<Texture2D>("Graphics/Industrial/tileset");
+                                foreach (Box box in map1.boxList)
+                                {
+                                    box.boxTexture = Content.Load<Texture2D>("Graphics/Industrial/box");
+                                }
+
+                            }
+                        }
+                    }
+
+                }
+                #endregion
             }
 
+            if (currentScreen == Screens.MENU)
+            {
+                menu.Draw(spriteBatch, font);
+            }
             spriteBatch.End();
 
             base.Draw(gameTime);
